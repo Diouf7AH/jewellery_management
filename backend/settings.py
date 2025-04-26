60350452acb105d863efd9f40254cd159dffa490
Diouf7AH <lamzooo555@gmail.com>
2025-04-26 04:29:07 +0000

    secket_key

--- backend/settings.py
+++ backend/settings.py
@@ -17,7 +17,7 @@ from decouple import config, Csv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', cast=str)
 DEBUG = config('DEBUG', cast=bool)
 # DEBUG = config('DEBUG', default=False, cast=bool)
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv)
