981410deff042aa96c4d44246913412eb0b0135f
Diouf7AH <lamzooo555@gmail.com>
2025-09-26 00:38:06 +0000

    inventory purchase migrations-04-6

--- staff/migrations/0001_initial.py
+++ staff/migrations/0001_initial.py
@@ -5,8 +5,9 @@ from django.conf import settings
 
 class Migration(migrations.Migration):
 
+    initial = True
+    
     dependencies = [
-        ('staff', '0001_initial'),
         ('store', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
