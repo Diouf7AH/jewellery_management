c60b9fdb7472c45b6a7e2df2b239f8ce70db0c99
Diouf7AH <lamzooo555@gmail.com>
2025-09-26 00:17:37 +0000

    inventory purchase migrations-04-3

--- staff/migrations/0003_initial.py
+++ staff/migrations/0003_initial.py
@@ -1,25 +1,22 @@
 from django.db import migrations, models
 import django.db.models.deletion
-from django.conf import settings
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        # mets ici la DERNIÈRE migration staff existante avant celle-ci
-        ('staff', '0002_initial'),
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('staff', '0002_cashier_user'),
+        ('store', '0001_initial'),     # on attend que 'bijouterie' existe
     ]
 
     operations = [
         migrations.AddField(
             model_name='cashier',
-            name='user',
-            field=models.OneToOneField(
-                to=settings.AUTH_USER_MODEL,
+            name='bijouterie',
+            field=models.ForeignKey(
+                to='store.bijouterie',
                 on_delete=django.db.models.deletion.SET_NULL,
-                null=True,                 # pratique si des Cashier existent déjà
-                related_name='cashier_profile',  # << placeholder résolu
-                db_index=True,
+                null=True, blank=True,
+                related_name='cashiers',      # valeur figée (pas de '%(class)ss')
             ),
         ),
     ]
