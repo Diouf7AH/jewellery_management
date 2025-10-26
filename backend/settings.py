1de70e21dd4ccdfed99ea678e5139534355f94f6
Diouf7AH <lamzooo555@gmail.com>
2025-10-26 23:20:25 +0000

    django-filter version

--- backend/settings.py
+++ backend/settings.py
@@ -114,21 +114,19 @@ TEMPLATES = [
 
 # WSGI_APPLICATION = 'backend.wsgi.application'
 
-# Pour que les décimales ressortent en chaînes ("25000.00", "95.500"), ajoute :
-# REST_FRAMEWORK = {"COERCE_DECIMAL_TO_STRING": True}
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework_simplejwt.authentication.JWTAuthentication',
-#     )
-# } 
+
 
 # --- DRF (fusion de tes deux blocs) ---
 REST_FRAMEWORK = {
     "COERCE_DECIMAL_TO_STRING": True,
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
     ),
+    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated"],
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 20,
 }
 
 
